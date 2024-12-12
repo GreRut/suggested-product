05238159364f1117e4db76917b72ea2e78133452
GreRut <ruta.grzegorz@gmail.com>
2024-12-12 09:40:56 +0100

    astro

--- Product.Api/obj/Debug/net8.0/Product.Api.AssemblyInfo.cs
+++ Product.Api/obj/Debug/net8.0/Product.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Product.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfa299ba3c8499bf2dc872c35ecd3523e84b1033")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3cc3676f6dacec28f528d40f6c639a032594ee6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Product.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Product.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
