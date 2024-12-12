fbf604f962d5e33ea1df7a1282b76496ff2ddc89
GreRut <ruta.grzegorz@gmail.com>
2024-12-12 11:55:46 +0100

    Database

--- Product.Api/obj/Debug/net8.0/Product.Api.AssemblyInfo.cs
+++ Product.Api/obj/Debug/net8.0/Product.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Product.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee928f58c6ddb641bda10e4181396925debed31d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d17501eea4f12e016b48282578a0b3654824be6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Product.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Product.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- second-star/src/content.config.ts
+++ second-star/src/content.config.ts
@@ -3,11 +3,15 @@ import { defineCollection, z } from 'astro:content';
 import { glob, file } from 'astro/loaders';
 
 const products = defineCollection({
-    loader: glob ({pattern: "**/*.json", base: "./src/data/products"}),
+    loader: glob ({pattern: "**/*.json", 
+    base: "./src/data/products"}),
     schema: z.object({
     name: z.string(),
-    maja: z.string()
-    })
+    description: z.string(),
+    price: z.string(),
+    id: z.string().uuid(),
+    imgSrc: z.string().url()
+    }),
 });
 
 // 4. Export a single `collections` object to register your collection(s)
