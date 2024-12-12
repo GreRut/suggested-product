9d434ae3799e5b18c2cbb4b1a88b02fc897181e5
GreRut <ruta.grzegorz@gmail.com>
2024-12-12 09:28:03 +0100

    jfbfb

---
+++ Product.Api/Controllers/HelloController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Product.Api.Controllers
+{
+    [ApiController, Route("api/1/[controller])")]
+    class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public string SayHello() => "Hello";
+    }
+}
